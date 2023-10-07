55dfa4eff9a101291ffbd02728ffc456d5808a06
Beata Krasnopolska <bkrasnopolska@objectivity.co.uk>
2023-10-07 13:56:26 +0200

    refactor and use bufio.NewScanner

--- blogposts.go
+++ blogposts.go
@@ -1,21 +1,34 @@
 package blogposts
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 	"io/fs"
+	"strings"
 )
 
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
+
+// Post represents a post on a blog
 type Post struct {
-	Title string
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
 }
 
+// NewPostsFromFS returns a collection of blog posts from a file system. If it does not conform to the format then it'll return an error
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
-
 	if err != nil {
 		return nil, err
 	}
-
 	var posts []Post
 	for _, f := range dir {
 		post, err := getPost(fileSystem, f)
@@ -34,11 +47,30 @@ func getPost(fileSystem fs.FS, f fs.DirEntry) (Post, error) {
 	}
 	defer postFile.Close()
 
-	postData, err := io.ReadAll(postFile)
-	if err != nil {
-		return Post{}, err
+	return newPost(postFile)
+}
+
+func newPost(postBody io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postBody)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
-	post := Post{Title: string(postData)[7:]}
-	return post, nil
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
 }
--- blogposts_test.go
+++ blogposts_test.go
@@ -17,21 +17,59 @@ func (s StubFailingFS) Open(name string) (fs.File, error) {
 }
 
 func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("Title: Post 1")},
-		"hello-world2.md": {Data: []byte("Title: Post 2")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := blogposts.NewPostsFromFS(fs)
 
-	got := posts[0]
-	want := blogposts.Post{Title: "Post 1"}
+	assertNoError(t, err)
 
+	assertPostsLength(t, posts, fs)
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func assertPostsLength(t *testing.T, posts []blogposts.Post, fs fstest.MapFS) {
+	t.Helper()
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %+v, want %+v", got, want)
-	}
-
-	if err != nil {
-		t.Fatalf("got ERROR %+v  ", err)
 	}
 }
